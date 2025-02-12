90c6bdb00c42c919b7ef7a974aa42f7e4a0eca9b
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-11 19:43:15 -0500

    added notes on callbacks and threads handler.ts

--- webapp/src/handler.ts
+++ webapp/src/handler.ts
@@ -10,6 +10,15 @@
 // };
 
 // Using the node.js API to read the contents of a file.
+// three callbacks in the code. One that is passed to the createServer function, which is invoked when
+// an http request is received.
+// Another callback is the one that is passed to the readFile function, which is invoked when
+// when the contents of the file have been read or if an error occurs.
+// Then the third callback is invoked when the data read from the file has been sent to the client
+
+//Breaking up the process of producing an http response with callbacks
+// means that the js main thread does not have to wait for the file system to read the contents of the file,
+// this allows requests from other clients to be procesed.
 import { IncomingMessage, ServerResponse } from "http";
 import { readFile } from "fs";
 
@@ -17,7 +26,7 @@ export const handler = (req: IncomingMessage, res:ServerResponse) => {
     readFile("data.json", (err: Error | null, data: Buffer) => {
         // reads the contents of a file.
         if (err == null) {
-            res.end(data, () => console.log("Filesent")); // comes up when user sends request
+            res.end(data, () => console.log("Filesent")); // comes up when user sends request. 3rd callback
         }
         //The read operation is asynchronous and is implemented using a native thread.
         //The contents of the file are passed to a callback function, which sends them to the http client.
